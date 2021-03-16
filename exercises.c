696197cbc1b615d15d6cf2a694160e5d3302fa13
Replit user <replituser@example.com>
2021-03-16 23:06:25 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -39,7 +39,8 @@ Utilice la función sumaN.
 void sumaNultimos(int a[], int n, int m, int * suma) {
   int sumaTotal = *suma;
   int sumaParcial;
-  n -= m-1;
+  n -= m;
+  n++;
   sumaParcial = sumaN(a,n);
   *suma =sumaParcial - sumaTotal;
 }
